9183ed53788f0f9d0ff2cde5632c7216649afd6e
bala <gnagurus@gmail.com>
2020-07-29 14:23:24 +0530

    refactor: added additional loggers to display the Player score info and extracted main method

--- src/main/java/com/bnpp/kata/TennisGameRunner.java
+++ src/main/java/com/bnpp/kata/TennisGameRunner.java
@@ -1,6 +1,7 @@
 package com.bnpp.kata;
 
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 import com.bnpp.kata.tennis.TennisGame;
 import com.bnpp.kata.tennis.exception.InvalidUserInputException;
@@ -9,26 +10,54 @@ import static com.bnpp.kata.tennis.utils.TennisGameConstants.*;
 
 public class TennisGameRunner {
 
-	public String playGame() throws InvalidUserInputException {
-		String gameScore;
+	private static final Logger LOGGER = Logger.getLogger(TennisGameRunner.class.getName());
+	
+	public static void main(String[] args) {
+		new TennisGameRunner().playGame();
+	}
+	
+	public String playGame() {
+		LOGGER.info("******** Welcome to play the Tennis Game! ********");
+		
+		String gameScore = "";
 		Scanner inputScanner = new Scanner(System.in);
-
+		
+		LOGGER.info("Please enter the player1 name");
 		String playerOneName = inputScanner.nextLine();
 
+		LOGGER.info("Please enter the player2 name");
+		
 		String playerTwoName = inputScanner.nextLine();
 
 		TennisGame tennisGame = new TennisGame(playerOneName, playerTwoName);
+		
+		String winningIndicatorMessage = String.format(
+				"Press 'A' to indicate '%s' win the current service or Press 'B' to indicate ' %s' win the current service",
+				playerOneName, playerTwoName);
 
+		LOGGER.info("******** Lets start the game ! ********");
+		
 		do {
-			char winningIndicator = inputScanner.next().charAt(0);
+			LOGGER.info(winningIndicatorMessage);
+			
+			try {
+				char winningIndicator = inputScanner.next().charAt(0);
 
-			tennisGame.addServicePointToWinner(String.valueOf(winningIndicator));
-			gameScore = tennisGame.calculateGameScore();
+				tennisGame.addServicePointToWinner(String.valueOf(winningIndicator));
+				gameScore = tennisGame.calculateGameScore();
+			} catch (InvalidUserInputException e) {
+				LOGGER.warning(e.getMessage());
+			}
 
+			String scoreDetails = String.format("Current Game score: %s", gameScore);
+
+			LOGGER.info(scoreDetails);
+			
 		} while (isGameInProgress(gameScore));
 
 		inputScanner.close();
-
+		
+		LOGGER.info("******** Game Over ! ********");
 		return gameScore;
 	}
 
